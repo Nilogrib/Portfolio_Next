681ece1949c4adc200aa2cb4919add89fd4567d0
Nilogrib <142620260+Nilogrib@users.noreply.github.com>
2024-12-19 21:09:47 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,21 +14,21 @@ const Contact = () => {
                 </li>
                 <li className='flex items-center'>
                     <Image src={mail} alt="mail" className='h-[110px] w-auto mr-6'/>
-                    <p className='text-xl'>nilogri&#39;s@gmail.com</p>
+                    <p className='text-xl'>nilogribghosh@gmail.com</p>
                 </li>
             </ul>
         </div>
         <div className='bg-white/10 p-6 rounded-xl max-w-[550px]'>
-            <h2 className='text-5xl font-bold text-orange-400 mb-4'>Let&#39;s connect</h2>
-            <p className='text-white/70 mb-6'>Send me a message and let&#39;s schedule a call</p>
+            <h2 className='text-5xl font-bold text-orange-400 mb-4'>Let's connect</h2>
+            <p className='text-white/70 mb-6'>Send me a message and let's schedule a call</p>
             <form className='space-y-4' action="https://getform.io/f/aqoowrla" method="POST">
                 <div className='grid md:grid-cols-2 gap-4'>
                     <input type="text" name="name" className='bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='First Name'/>
-                    <input type="text" name="name" className='bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Last Name'/>
+                    <input type="text" name="last_name" className='bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Last Name'/>
                     <input type="email" name="email" className='bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Email'/>
                     <input type="phone" name="phone" className='bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Phone'/>
                 </div>
-                <textarea className='bg-black/70 w-full rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Your Message'/>
+                <textarea className='bg-black/70 w-full rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400' placeholder='Your Message'></textarea>
                 <button className='bg-orange-700 hover:bg-orange-500 text-white px-6 py-2 w-full font-semibold text-xl rounded-xl'>Send Message</button>
             </form>
         </div>
@@ -36,4 +36,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
