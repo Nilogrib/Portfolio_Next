a573c5d22fc2f4d0fea2b883f8d6f79d93f330e8
Nilogrib <142620260+Nilogrib@users.noreply.github.com>
2024-12-30 22:36:37 +0530

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -13,7 +13,7 @@ const projects = [
     devStack: "HTML, CSS, React JS, Mongo DB, Express JS, Node JS",
     link: "https://food-del-frontend-9yw5.onrender.com/",
     git: "https://github.com/Nilogrib/food-del",
-    src: proj1
+    src: proj1,
   },
   {
     title: "Project 2",
@@ -21,7 +21,7 @@ const projects = [
     devStack: "Python, Streamlit, scikit-learn, Pandas, NTLK",
     link: "https://github.com/Nilogrib/Moviepedia",
     git: "https://github.com/Nilogrib/Moviepedia",
-    src: proj3
+    src: proj3,
   },
   {
     title: "Portfolio",
@@ -29,22 +29,24 @@ const projects = [
     devStack: "HTML, React JS, Tailwind CSS, Next JS, Framer Motion",
     link: "https://portfolio-next-1guf.vercel.app/",
     git: "https://github.com/Nilogrib/Portfolio_Next",
-    src: proj2
-  }
-]
+    src: proj2,
+  },
+];
 
 const Portfolio = () => {
   return (
     <div className="text-white bg-gradient-to-b from-black to-[#381a5f] py-18 mt-52" id="portfolio">
-      <h1 className="text-white text-6xl w-[320px] mx-auto font-semibold my-12">Selected<span className="text-orange-400"> Projects</span></h1>
+      <h1 className="text-white text-6xl w-[320px] mx-auto font-semibold my-12">
+        Selected<span className="text-orange-400"> Projects</span>
+      </h1>
       <div className="px-6 md:px-0 max-w-[1000px] mx-auto mt-40 space-y-24">
         {projects.map((project, index) => (
           <motion.div
             key={index}
-            initial={{opacity:0,y:75}}
-            whileInView={{opacity:1,y:0}}
-            viewport={{once:true}}
-            transition={{duration:0.5, delay:0.25}}
+            initial={{ opacity: 0, y: 75 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.25 }}
             className={`mt-12 flex flex-col ${index % 2 === 1 ? "md:flex-row-reverse gap-12" : "md:flex-row"}`}
           >
             <div className="space-y-2 max-w-[550px]">
@@ -53,23 +55,25 @@ const Portfolio = () => {
               <p className="text-lg text-white/70 break-words p-4">{project.desc}</p>
               <p className="text-xl text-orange-400 font-semibold">{project.devStack}</p>
               <div className="w-64 h-[1px] bg-gray-400 my-4"></div>
-              <a href={project.link} className="mr-6">Link</a>
+              <a href={project.link} className="mr-6">
+                Link
+              </a>
               <a href={project.git}>Github</a>
             </div>
             <div className="flex justify-center items-center">
-              <Image 
-                src={project.src} 
-                alt={project.title} 
-                width={500} 
-                height={350} 
-                className="w-[500px] h-[350px] object-cover border rounded border-gray-700"
+              <Image
+                src={project.src}
+                alt={project.title}
+                width={600} // Increased width
+                height={400} // Increased height
+                className="w-[600px] h-[400px] object-cover border rounded border-gray-700"
               />
             </div>
           </motion.div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Portfolio;
